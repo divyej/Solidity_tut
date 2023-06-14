13a757e12772a126ad194932411b64ef99bf3f04
Divye <djain94314@gmail.com>
2023-06-15 00:48:23 +0530

    ads

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract MyContract {
-    constructor() {}
+contract VotingSystem{
+  struct Candidate {
+    string Name;
+    uint256 voteCount;
+    }
+    mapping(uint256=>Candidate) public candidates;
+    uint256 public candidatesCount;
+    constructor(string[] memory _candidatesNames){
+        candidatesCount= _candidatesNames.length;
+        for(uint256 i=0;i<candidatesCount;
+        i++){
+            candidates[i+1]=Candidate({
+                Name: _candidatesNames[i],
+                voteCount:0
+            });
+        }
+    }
 }
