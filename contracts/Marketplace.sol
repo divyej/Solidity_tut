bdae283ced24531c09df303a67fc357a0ded9ecb
Divye <djain94314@gmail.com>
2023-06-15 21:09:18 +0530

    marketpladce

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -2,7 +2,45 @@
 pragma solidity ^0.8.9;
 
 contract MarketPlace{
+     struct User{
+        string name;
+        bool isRegistered;
+     }
+     struct Item{
+        uint256 id;
+        string name;
+        string description;
+        uint256 quantity;
+        uint256 price;
+        address payable seller;
+        address payable buyer;
+        bool isSold;
+     }
+     mapping(address=>User)public users;
+     Item[] public items;
+     event ItemListed(uint256 itemId, string itemName , uint256 price);
+     event ItemPurchased(uint256 itemId, string itemName, address buyer);
      
+     modifier onlyRegisteredUser(){
+        require(users[msg.sender].isRegistered,"only registered user are allowed");
+        _;
+     }
+      constrtuctor(){
+    
+     }
+    function registeredAsUser(string memory _name) public{
+        require(!users[msg.sender].isRegistered,"User already registered"
+        );
+        users[msg.sender]=User(_name,true);
+    }
+    function purchaseItem(uint256 _itemId) public payable onlyRegisteredUser {
+        Item storage selectedItems = items[_itemId];
+        require(!selectedItems.isSold,"item has alread)
+
+    }
+
+
+
 }
 
 
