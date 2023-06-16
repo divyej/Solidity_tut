a4f99c4cd94996cb81be335ead39188b765f6fbb
Divye <djain94314@gmail.com>
2023-06-16 21:02:38 +0530

    added projects creation

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -14,5 +14,28 @@ uint256 contributionCount;
 bool isCompleted;
 
 }
+uint256 public projectsCount;
+mapping(uint256 => Project) public projects;
+
+
+event projectCreated(uint256 projectId,string title,uint256 goalAmount);
+event contributionMade(uint256 projectId, address contributor, uint256 amopunt );
+event projectCompleted(uint256 projextId, string title, uint256 totalAmount);
+
+function createProject(string memory _title, string memory _description, uint256 _goalAmount ) public{
+  projectsCount++;
+  projects[projectsCount].id=projectsCount;
+  projects[projectsCount].creator=msg.sender;
+  projects[projectsCount].title=_title;
+  projects[projectsCount].description=_description;
+  projects[projectsCount].goalAmount=_goalAmount;
+  projects[projectsCount].currentAmount =0;
+  projects[projectsCount].contributionCount=0;
+  projects[projectsCount].contributionCount=0;
+  projects[projectsCount].isCompleted=false;
+emit projectCreated(projectsCount, _title, _goalAmount);
+}
+
+
 
 }
