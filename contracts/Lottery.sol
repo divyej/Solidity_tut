631190f99d93afe3326870271671ce09f77e99d3
Divye <djain94314@gmail.com>
2023-06-17 01:33:03 +0530

    added functionalities

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -12,6 +12,11 @@ contract Lottery{
     event TicketBought(address indexed player ,uint256 amount);
     event winnerSelected(address winner , uint256 prize);
     
+    modifier restricted(){
+        require(msg.sender==managers,"Only the manager can call this function");
+        _;
+    }
+    
     function buyTicket() public payable{
         require(msg.value>0.01 ether,"insufficinet funds");
         players.push(payable(msg.sender));
@@ -20,5 +25,13 @@ contract Lottery{
 
     function random() private view returns (uint256){
         return uint256(keccak256(abi.encodePacked(block.timestamp,block.difficulty,players.length)));
+    }
+    function selectWinner() public restricted{
+        require(players.length>0,"no players in this lottery");
+        uint256 index = random() % players.length;
+        address payable winner = players[index];
+        winner.transfer(address(this).balance);
+
+        players = new address payable[](0);
     }
 }
