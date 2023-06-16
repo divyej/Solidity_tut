e6cc4d67ed24813387abcf1f616a5ef95b8a2024
Divye <djain94314@gmail.com>
2023-06-17 01:05:24 +0530

    lottery

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,2 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
+
+contract Lottery{
+    address public managers;
+    address payable[]  public players;
+
+    constructor(){
+        managers=msg.sender;
+    }
+
+    event TicketBought(address indexed player ,uint256 amount);
+    event winnerSelected(address winner , uint256 prize);
+    
+    function buyTicket() public payable{
+        require(msg.value>0.01 ether,"insufficinet funds");
+        players.push(payable(msg.sender));
+        emit TicketBought(msg.sender, msg.value);
+    }
+
+    function random() private view returns (uint256){
+        
+
+    }
+}
